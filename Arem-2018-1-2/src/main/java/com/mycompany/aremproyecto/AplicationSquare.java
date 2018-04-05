0193d930562789bc6fbb70259268ae5104b1976f
danielagon <daniela.gonzalezramirez@hotmail.com>
2018-04-04 22:40:37 -0500

    Carpeta Arem-2018-1-2

---
+++ Arem-2018-1-2/src/main/java/com/mycompany/aremproyecto/AplicationSquare.java
@@ -0,0 +1,22 @@
+package com.mycompany.aremproyecto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Guzman
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationSquare {
+    public static void main(String[] args) {
+		SpringApplication.run(AplicationSquare.class, args);
+	}
+}
+
